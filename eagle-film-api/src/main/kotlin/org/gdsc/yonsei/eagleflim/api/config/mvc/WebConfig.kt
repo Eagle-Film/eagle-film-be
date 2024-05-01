ced5b350962d182ae81fdca328129abb0b0aeaa0
VSFe <klm03025@gmail.com>
2024-05-02 03:48:49 +0900

    chore: 임시적으로 CORS 해제

--- eagle-film-api/src/main/kotlin/org/gdsc/yonsei/eagleflim/api/config/mvc/WebConfig.kt
+++ eagle-film-api/src/main/kotlin/org/gdsc/yonsei/eagleflim/api/config/mvc/WebConfig.kt
@@ -5,13 +5,14 @@ import org.gdsc.yonsei.eagleflim.api.resolver.EagleUserArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 open class WebConfig(
-	val eagleUserArgumentResolver: EagleUserArgumentResolver,
-	val jwtInterceptor: JwtInterceptor
+	private val eagleUserArgumentResolver: EagleUserArgumentResolver,
+	private val jwtInterceptor: JwtInterceptor
 ) : WebMvcConfigurer {
 	override fun addFormatters(registry: FormatterRegistry) {
 		super.addFormatters(registry)
@@ -28,5 +29,14 @@ open class WebConfig(
 	override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
 		super.addArgumentResolvers(resolvers)
 		resolvers.add(eagleUserArgumentResolver)
+	}
+
+	override fun addCorsMappings(registry: CorsRegistry) {
+		// TODO: 개발 과정에서만 전체 해제. 이후에는 CORS 예외 규칙을 완전히 제거함.
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowCredentials(true)
+			.maxAge(3600L)
 	}
 }
