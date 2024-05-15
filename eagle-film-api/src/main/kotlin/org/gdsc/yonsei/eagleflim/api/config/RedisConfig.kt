50bf67c42be1836142dc2c29bb5652a48464df9f
VSFe <klm03025@gmail.com>
2024-05-15 13:24:31 +0900

    feat: Redis 관련 설정 추가

---
+++ eagle-film-api/src/main/kotlin/org/gdsc/yonsei/eagleflim/api/config/RedisConfig.kt
@@ -0,0 +1,19 @@
+package org.gdsc.yonsei.eagleflim.api.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
+@Configuration
+open class RedisConfig(
+	@Value("\${spring.data.redis.host}") private val host: String,
+	@Value("\${spring.data.redis.port}") private val port: Int
+) {
+	@Bean
+	open fun redisConnectionFactory(): RedisConnectionFactory {
+		return LettuceConnectionFactory(host, port)
+	}
+}
+
