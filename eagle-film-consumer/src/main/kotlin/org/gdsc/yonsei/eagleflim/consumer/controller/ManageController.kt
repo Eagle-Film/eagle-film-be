fa6d3564d65ca5e3d5747737d28b3d7865db44bc
VSFe <klm03025@gmail.com>
2024-05-15 13:26:59 +0900

    feat: Manage Controller

---
+++ eagle-film-consumer/src/main/kotlin/org/gdsc/yonsei/eagleflim/consumer/controller/ManageController.kt
@@ -0,0 +1,31 @@
+package org.gdsc.yonsei.eagleflim.consumer.controller
+
+import org.gdsc.yonsei.eagleflim.consumer.controller.model.NodeInput
+import org.gdsc.yonsei.eagleflim.consumer.service.NodeService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/consumer/v1/manage")
+class ManageController(
+	private val nodeService: NodeService
+) {
+	@PostMapping("/node")
+	fun registerNode(@RequestBody nodeInput: NodeInput) {
+		nodeService.addNode(nodeInput.address)
+	}
+
+	@DeleteMapping("/node")
+	fun deleteNode(@RequestBody nodeInput: NodeInput) {
+		nodeService.removeNode(nodeInput.address)
+	}
+
+	@GetMapping("/status")
+	fun scan() {
+
+	}
+}
