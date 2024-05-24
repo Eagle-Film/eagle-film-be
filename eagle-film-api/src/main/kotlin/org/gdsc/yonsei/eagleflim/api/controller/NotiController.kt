400bad9c49bc9d0ef14aaab6fd57a9fd79eb4653
bc.kim <bc.kim@navercorp.com>
2024-05-24 15:33:52 +0900

    fix: 버그 수정

--- eagle-film-api/src/main/kotlin/org/gdsc/yonsei/eagleflim/api/controller/NotiController.kt
+++ eagle-film-api/src/main/kotlin/org/gdsc/yonsei/eagleflim/api/controller/NotiController.kt
@@ -6,9 +6,11 @@ import org.gdsc.yonsei.eagleflim.api.service.NotiService
 import org.gdsc.yonsei.eagleflim.common.model.UserInfo
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1/noti")
 class NotiController(val notiService: NotiService) {
 	@PostMapping
 	fun createNoti(@EagleUser userInfo: UserInfo, @RequestBody notiRequestInput: NotiRequestInput) {
